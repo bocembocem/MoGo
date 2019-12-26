d0d650e9596c0bdcdae3aa86ee198323de514422
bocembocem <nzolotykh94@mail.ru>
2019-12-26 17:32:55 +0400

    continue section service2

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
 $(function() { 
-    
+    $('.service2-content-accordeon-toggle').first().slideDown();
+    $('.service2-content-accordeon-tab').on('click', function() {
+        $('.service2-content-accordeon-toggle').slideUp();
+        $(this).next().slideToggle();
+    });
 });
